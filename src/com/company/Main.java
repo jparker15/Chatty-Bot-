74d6ba8bfc3bfd176369769ae4a0aa35f82c88f7
Jeremy <jeremyeparker@yahoo.com>
2021-07-06 23:53:40 -0400

    todo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("I am chatty bot");
+        System.out.println("Data I was created");
+        System.out.println("Ask for your name?!");
     }
 }
