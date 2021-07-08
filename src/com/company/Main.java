8d6f78b1f298d6d1118920675af4bfb854a706b9
Jeremy <jeremyeparker@yahoo.com>
2021-07-07 23:57:01 -0400

    add data created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,16 @@
 package com.company;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("I am chatty bot");
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        System.out.println(formatter.format(date));
         System.out.println("Data I was created");
         System.out.println("Ask for your name?!");
     }
